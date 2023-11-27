16305cafbcf996fe97f9dd9d91b1b353e89ab19f
Entroshock <alextsang@live.ie>
2023-11-27 23:47:26 +0000

    getting the account creation working

--- Backend/app/model/db.js
+++ Backend/app/model/db.js
@@ -1,16 +1,31 @@
-const mysql = require('mysql2');
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config();
+
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "j34567yulittle",
-    database: "mydb",
-    port: 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DATABASE_PORT
+}).promise();
 
-exports.createUser = (userData, callback) => {
-    pool.query('INSERT INTO createuser SET ?', userData, callback);
-};
+
+async function queryDatabase() {
+    try {
+        const [rows] = await pool.query("SELECT * FROM createUser");
+        console.log(rows);
+    } catch (error) {
+        console.error('Error executing query:', error);
+    }
+}
+
+//prepared statement
+export async function createAccount(userEmail, userFirstName, userLastName,userName,userPw){
+    const [result] = await pool.query(
+        'INSERT INTO createUser(userEmail, userFirstName,userLastName,userName,userPw) VALUES (?,?,?,?,?)',
+        [userEmail, userFirstName, userLastName,userName,userPw])
+}
+
+ 
